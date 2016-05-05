8a494ea3329e035d66ae351ffdc378b770c112b1
Conor O'Connell <cono@pdx.edu>
2016-05-04 22:16:21 -0700

    Added and tested Register file module.

---
+++ RegesterFile/regfile.v
@@ -0,0 +1,33 @@
+/* PURPOSE: This module simulates a three port register file (2 output and 1 input port).
+*  INPUTS :Clock, write Enable, write address, write data, read address(port A), read address(port B)
+*  OUTPUTS:Read data(port A), Read data(port B)
+*  Conor O'Connell
+*  ECE 486
+*/
+
+//http://courses.cs.washington.edu/courses/cse370/10sp/pdfs/lectures/regfile.txt
+
+
+module regfile ( clk,  writeEnable, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
+   parameter NUM_ADDR_BITS = 6;
+   parameter REG_WIDTH = 32;
+   parameter NUM_REGS = 2**NUM_ADDR_BITS;
+   
+   input clk;
+   input writeEnable;
+   input [NUM_ADDR_BITS - 1:0] wrAddr;
+   input [REG_WIDTH - 1:0]     wrData;
+   input [NUM_ADDR_BITS - 1:0] rdAddrA;
+   output [REG_WIDTH - 1:0]    rdDataA;
+   input [NUM_ADDR_BITS - 1:0] rdAddrB;
+   output [REG_WIDTH - 1:0]    rdDataB;
+
+   reg [REG_WIDTH - 1:0] 	 regfile [0:NUM_REGS - 1];
+
+   assign rdDataA = rdAddrA ? regfile[rdAddrA] : 32'h00000000;
+   assign rdDataB = rdAddrB ? regfile[rdAddrB] : 32'h00000000;
+
+   always @(posedge clk) begin
+      if (writeEnable) regfile[wrAddr] <= wrData;
+   end
+endmodule
---
+++ RegesterFile/regfile_tester.v
@@ -0,0 +1,69 @@
+/* PURPOSE: Test bed for regfile
+ * INPUT: None
+ * OUTPUT: None
+ * Conor O'Connell
+ */
+
+module regfiletester();
+
+parameter NUM_ADDR_BITS = 6;
+parameter REG_WIDTH = 32;
+
+reg clk;
+reg writeEnable;
+reg [NUM_ADDR_BITS - 1:0] wrAddr;
+reg [REG_WIDTH - 1:0] wrData;
+reg [NUM_ADDR_BITS - 1:0] rdAddrA;
+reg [NUM_ADDR_BITS - 1:0] rdAddrB;
+wire [REG_WIDTH - 1:0] rdDataA;
+wire [REG_WIDTH - 1:0] rdDataB;
+
+regfile RF_1( clk,  writeEnable, wrAddr, wrData, rdAddrA, rdDataA, rdAddrB, rdDataB);
+
+initial
+repeat (50) 
+  begin
+    #2 clk = !clk;
+  end
+ 
+initial 
+  begin
+    #2
+    clk = 0;
+    writeEnable = 'b1;
+    wrAddr = 'h1;
+    wrData = 'h14578BB0;
+    #4
+    writeEnable = 'b0;
+    wrAddr = 'h2;
+    wrData = 'h00000001;
+    rdAddrA = 'h1;
+    #4
+    writeEnable = 'b1;
+    wrAddr = 'h2;
+    wrData = 'hFFFFFFFF;
+    rdAddrA = 'h1;
+    rdAddrB = 'h2;
+    #4
+    writeEnable = 'b1;
+    wrAddr = 'h0;
+    wrData = 'h88888888;
+    rdAddrA = 'h3F;
+    rdAddrB = 'h2;
+    #4
+    writeEnable = 'b1;
+    wrAddr = 'h3F;
+    wrData = 'h1;
+    rdAddrA = 'h3F;
+    rdAddrB = 'h0;
+    #4
+    writeEnable = 'b1;
+    wrAddr = 'h0;
+    wrData = 'hDDDDDDDD;
+    rdAddrA = 'h3F;
+    rdAddrB = 'h2;
+    #6
+    $finish();
+  end
+
+endmodule
