afbba60723ab37004112b3e597a986cd33ee0f5d
sarojbardewa <saroj@pdx.edu>
2016-05-10 23:23:02 -0700

    Debugged the cpu module

--- CPU_NN/cpu.v
+++ CPU_NN/cpu.v
@@ -8,40 +8,40 @@ module cpu(CLK);
 	// IF variables
 	wire [BUS_WIDTH-1:0] PCplus1;
 	reg [BUS_WIDTH-1:0] F_PC;
-	reg [BUS_WIDTH-1:0]Instr;
+	wire [BUS_WIDTH-1:0]Instr;
 	reg PCEn;
 	reg [BUS_WIDTH-1:0] InstrD;
 	
 		
-	//Instantiate Instruction Memory
-	instructionMemory IM1 (F_PC,Instr);
-	
 	//IF initial
 	initial begin
 		F_PC = 0;
 		CLK_cycle = 0;
-		PCEn = 1;	
-		Instr = 0;
 	end
 
-	assign PCplus1 = F_PC +1;
+//Instantiate Instruction Memory
+	instructionMemory IM1 (F_PC,Instr);
+	
 	always@(posedge CLK)
-		begin
-			CLK_cycle = CLK_cycle + 1;
-			
-			if(PCEn && ~Instr)
-				F_PC = 0;
-			else if (PCEn)
-				F_PC = PCplus1;
-			else
-				$display(" Must be an error!");
-		end
-
-	// IF Pipeline register
+		CLK_cycle = CLK_cycle + 1;
+	
 	always@(posedge CLK)
 		begin 
 			InstrD = Instr;
-			$display(" Cycle: %d, PC: %d, Instr: %h, InstrD: %h", CLK_cycle,F_PC,Instr,InstrD);
 		end
-			
+	always @(Instr)
+		begin
+			$display(" Cycle: %d, PC: %d, Instr: %h, InstrD: %h", CLK_cycle,F_PC,Instr,InstrD);
+			PCEn = 1;
+		end
+		
+				
+	always@(posedge CLK)
+		begin
+			if(PCEn)
+				F_PC = F_PC+1;
+		end
+	
+	// IF Pipeline register
+	
 endmodule
