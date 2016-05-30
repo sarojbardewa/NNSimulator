119c5e5327b3204862cfd1a84abdd2cb84b5d34f
sarojbardewa <saroj@pdx.edu>
2016-05-30 10:55:43 -0700

    Modified Sign Extender and Added more comments

---
+++ CPU_NN/BitFieldExtender.v
@@ -0,0 +1,25 @@
+/*********************************
+*  Sign Extender unit for NNsimulator project
+*  ECE 486 (computer architecture) final project
+*  PURPOSE: 
+*          This module sign extends inputs (usually immediate values) up to the size
+*          expected by the ALU (32 bits, in our case).
+* May 3rd, 2016
+* Conor O'Connell 
+**********************************/
+
+//http://stackoverflow.com/questions/4176556/how-to-sign-extend-a-number-in-verilog
+
+module bitFieldExt(valin, extended );
+parameter size_in = 16;
+parameter size_out = 32;
+
+input [size_in - 1:0] valin;
+output [size_out - 1:0] extended;
+
+wire [size_in - 1:0] valin;
+
+reg temp = 0;
+assign  extended[size_out - 1:0] = { {( size_out - size_in ){temp}}, valin[size_in-1:0] };
+
+endmodule
--- CPU_NN/cpu.v
+++ CPU_NN/cpu.v
@@ -1,94 +1,98 @@
+/*****************************************************************
+   This is 3-Stage Pipeline Neural Network Processor Module.
+   It interconnects different modules together to form
+   a processor.
+   
+   INPUT : Global clock 
+   OUTPUT: Cycle counter value
+   
+   Author: Saroj Bardewa
+   ECE586 Spring 2016
+ ****************************************************************/
 module cpu(CLK,CLK_cycle);
-	parameter BUS_WIDTH=32;
-	parameter IMMEDIATE = 16;  // No of bits in immediate field
-	parameter REGISTER = 6;	   // Size of Registers
-	parameter OPCODE = 4; 	   // Size of OPCODE
-	parameter ALU_FUNCT_BITS = 3;  // For Alu operations
+	parameter BUS_WIDTH=32;					// Width of the bus
+	parameter IMMEDIATE = 16; 				// No of bits in immediate field
+	parameter REGISTER = 6;	   				// Size of Registers
+	parameter OPCODE = 4; 	   				// Size of OPCODE
+	parameter ALU_FUNCT_BITS = 3;   		// For Alu operations
 	
-	input CLK;
+	input CLK;								// Global Clock
 	
 	// Cycle counter
-	output reg [BUS_WIDTH-1:0] CLK_cycle;
+	output reg [BUS_WIDTH-1:0] CLK_cycle;	
 
-	// IF variables
+	// Instruction Fetch variables
 	reg [BUS_WIDTH-1:0] PC;
 	wire [BUS_WIDTH-1:0]Instr;
-//	reg PCEn;
 	reg [BUS_WIDTH-1:0] InstrD;
-//--------------------------------------------
-	// Inputs to ID_EX Registers
-	wire [REGISTER-1:0] A1;
-	wire [REGISTER-1:0] A2;
-	wire [REGISTER-1:0] A3;
-	wire [REGISTER-1:0] A4;
-	wire [BUS_WIDTH-1:0] WD3;
-	wire [REGISTER-1:0] WriteDstReg;
-	wire [OPCODE-1:0] OP;
+
+	// Register File inputs
+	wire [REGISTER-1:0] 	A1;					//Register File input1
+	wire [REGISTER-1:0] 	A2;					//Register File input2
+	wire [REGISTER-1:0] 	A3;					//Register File input3
+	wire [REGISTER-1:0] 	A4;					//Register File input4
+	wire [BUS_WIDTH-1:0] 	WD;			        //Register File Write Data
+	wire [REGISTER-1:0] 	WriteDstReg;		//Temporary Write Destination Register
+	wire [OPCODE-1:0] 		OP;					//Control Unit Opcode
 	
-	// Register Files Input and output
+	//// Input and Output to Instruction Decode-Execute Pipeline Register
 	wire RegWriteD,RegWrite; 					//Write Enable Wire
-	wire [BUS_WIDTH-1:0]Src1AD,Src1A;
-	wire [BUS_WIDTH-1:0]Src1BD,Src1B;
-	wire [BUS_WIDTH-1:0]Src1CD,Src1C;
+	wire [BUS_WIDTH-1:0]Src1AD,Src1A;			// Source-A into and out of pipeline register
+	wire [BUS_WIDTH-1:0]Src1BD,Src1B;			// Source-B into and out of pipeline register
+	wire [BUS_WIDTH-1:0]Src1CD,Src1C;			// Source-C into and out of pipeline register
 	
-	//Sign Extend
-	wire [BUS_WIDTH-1:0] SignImmD,SignImm;
-	wire [IMMEDIATE-1:0] SignImmIn;
+	//Bit Field Extension
+	wire [BUS_WIDTH-1:0] BitFieldImmD,BitFieldImm;    	//output of sign extend module and pipeline register
+	wire [IMMEDIATE-1:0] BitFieldImmIn;					//Input to sign extend module
 	
 	//Control Unit
-	wire ALU1SrcD,ALU1Src;
-	wire RegDstD,RegDst;
-	wire [ALU_FUNCT_BITS-1:0]ALU1CntrlD,ALU1Cntrl;
-	wire [ALU_FUNCT_BITS-1:0]ALU2CntrlD,ALU2Cntrl;
-	wire MemWriteD,MemWrite;
-	wire MemtoRegD,MemtoReg;
-	wire PCEnD,PCEn;
-	wire [REGISTER-1:0]RtD,Rt;
-	wire [REGISTER-1:0]RdD,Rd;
+	wire ALU1SrcD,ALU1Src;								// ALU1 input1 
+	wire RegDstD,RegDst;								// Register Destination into and out of pipeline register
+	wire [ALU_FUNCT_BITS-1:0]ALU1CntrlD,ALU1Cntrl;		// ALU1 control into and out of pipeline register
+	wire [ALU_FUNCT_BITS-1:0]ALU2CntrlD,ALU2Cntrl;		// ALU2 control into and out of pipeline register
+	wire MemWriteD,MemWrite;							// Data memory write signal into and out of pipeline register
+	wire MemtoRegD,MemtoReg;							// Register or Memory output select signal into and out of pipeline register
+	wire PCEnD,PCEn;									// PC Enable signal into and out of pipeline register
+	wire [REGISTER-1:0]RtD,Rt;							// Destination register for I-type instruction into and out of pipeline register
+	wire [REGISTER-1:0]RdD,Rd;							// Destination register for R-type instruction into and out of pipeline register
 	
 	//Execution Unit
-	wire [BUS_WIDTH-1:0]Src1B1;  //Output of ALUSrc Mux
-	wire [BUS_WIDTH-1:0]ALUOut1, ALUOut2; //Output of ALU1
-	wire [BUS_WIDTH-1:0] WriteData;
-	wire [BUS_WIDTH-1:0] RdDataM;
-	wire [BUS_WIDTH-1:0] Result;
-	//-------------------------------------------------------------
+	wire [BUS_WIDTH-1:0]Src1B1;  						// Output of ALUSrc Mux
+	wire [BUS_WIDTH-1:0]ALUOut1, ALUOut2; 				// Output of ALU1
+	wire [BUS_WIDTH-1:0] WriteData;						// Data to write to data memory
+	wire [BUS_WIDTH-1:0] RdDataM;						// Output of data memory
+	wire [BUS_WIDTH-1:0] Result;						// Output of write back multiplexer
+
+	//---------------------------------------------------------------------------//
 	//IF initial
 	initial begin
 		PC = 0;
 		CLK_cycle = 0;
-	//	InstrD = 32'b0;
-	//	PCEn = 0; 
 	end
 
-	//===========================================================================
-	// Instruction Fetch
+	//===========================================================================//
+	// Instruction Fetch Unit
 	//Instantiate Instruction Memory
 	instructionMemory IM1 (PC,Instr);
 	
-	//Assign statements
-	assign OP = InstrD[31:28];
-	assign A1 = InstrD[27:22];
-	assign A2 = InstrD[21:16];
-	assign A3 = InstrD[15:10];
-	assign A4 = WriteDstReg;
-	assign SignImmIn = InstrD[IMMEDIATE-1:0];
-	assign RtD = InstrD[21:16];
-	assign RdD = InstrD[15:10];
+	//Assign the Instruction bits into respective fields
+	assign OP 	= InstrD[31:28];
+	assign A1 	= InstrD[27:22];
+	assign A2 	= InstrD[21:16];
+	assign A3	 = InstrD[15:10];
+	assign A4 	= WriteDstReg;
+	assign RtD 	= InstrD[21:16];
+	assign RdD 	= InstrD[15:10];
+	assign WD   = Result;          	//Assign write back value to the register input
 	assign WriteData = Src1B;
-	assign WD3	 = Result;
 	
+	assign BitFieldImmD  = InstrD[IMMEDIATE-1:0]; 
 
+	//Update the global clock counter
 	always@(posedge CLK)
 		CLK_cycle = CLK_cycle + 1;
-	
-	/*
-	always @(Instr)
-		begin
-			$display(" Cycle: %d, PC: %d, Instr: %h, InstrD: %h", CLK_cycle,PC,Instr,InstrD);
-		end
-	*/	
-				
+
+	//Update the program counter by 1
 	always@(posedge CLK)
 		begin
 			if(PCEn)
@@ -101,27 +105,27 @@ module cpu(CLK,CLK_cycle);
 				
 		end
 	// Instantiate Register File
-	regfile ArtNNRegisterUnit(.clk(CLK),.writeEnable(RegWrite),.wrAddr(A4),.wrData(WD3),
+	regfile ArtNNRegisterUnit(.clk(CLK),.writeEnable(RegWrite),.wrAddr(A4),.wrData(WD),
 					  .rdAddrA(A1),.rdDataA(Src1AD),.rdAddrB(A2),.rdDataB(Src1BD),
 					  .rdAddrC(A3),.rdDataC(Src1CD));
 	
 	
-	// sign extended
-	signEx ArtNNSignExtUnit(.valin(SignImmIn),.extended(SignImmD));
+	// Bit Field extended -16bits input and 32-bit output
+	bitFieldExt ArtNNBitFieldExtUnit(.valin(BitFieldImmIn),.extended(BitFieldImmD));
 	
 	//Control Unit
 	controlUnit ArtNNControlUnit(.opcode(OP), .RegWrite(RegWriteD), .MemtoReg(MemtoRegD), .MemWrite(MemWriteD), .ALUControl1(ALU1CntrlD),
                 .ALUControl2(ALU2CntrlD), .ALUSrc(ALU1SrcD),.RegDst(RegDstD),.PCEn(PCEnD));
 	
 	// ID_EX Register
-	IFEX_Reg ArtNNIFEXRegUnit(CLK,PCEnD,RegWriteD, ALU1SrcD, RegDstD,ALU1CntrlD, ALU2CntrlD,MemWriteD,MemtoRegD,Src1AD,Src1BD,Src1CD,RtD,RdD,SignImmD,
-					PCEn,RegWrite,ALU1Src, RegDst,ALU1Cntrl, ALU2Cntrl,MemWrite,MemtoReg,Src1A,Src1B,Src1C,Rt,Rd,SignImm);
+	IFEX_Reg ArtNNIFEXRegUnit(CLK,PCEnD,RegWriteD, ALU1SrcD, RegDstD,ALU1CntrlD, ALU2CntrlD,MemWriteD,MemtoRegD,Src1AD,Src1BD,Src1CD,RtD,RdD,BitFieldImmD,
+					PCEn,RegWrite,ALU1Src, RegDst,ALU1Cntrl, ALU2Cntrl,MemWrite,MemtoReg,Src1A,Src1B,Src1C,Rt,Rd,BitFieldImm);
 	
 	// 2 input mux to select write Destination Register
 	mux32x2_6bits ArtNNMux_RegDest(.in0(Rt), .in1(Rd),.select(RegDst),.out(WriteDstReg));
 	
 	// 2 input mux to select ALU1 Source
-	mux32x2 ArtNNMux_ALU1Src(.in0(Src1B), .in1(SignImm),.select(ALU1Src),.out(Src1B1));
+	mux32x2 ArtNNMux_ALU1Src(.in0(Src1B), .in1(BitFieldImm),.select(ALU1Src),.out(Src1B1));
 
 	ALU_Parent ALU1(.ALUResult(ALUOut1),.ALU1Control(ALU1Cntrl),.SrcA(Src1A),.SrcB(Src1B1));
 	
